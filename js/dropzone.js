80293457c710d47062adc8e2202ce68f0f071f7c
Craig Peden <craig.peden.mail@gmail.com>
2013-04-06 03:31:02 +0100

    dropzone

--- js/dropzone.js
+++ js/dropzone.js
@@ -528,7 +528,7 @@ require.register("dropzone/lib/dropzone.js", function(exports, require, module){
         return file.previewTemplate.classList.add("success");
       },
       complete: noop,
-      previewTemplate: "<div class=\"preview file-preview\">\n  <div class=\"details\">\n   <div class=\"filename\"><span></span></div>\n  </div>\n  <div class=\"progress\"><span class=\"upload\"></span></div>\n  <div class=\"success-mark\"><span>✔</span></div>\n  <div class=\"error-mark\"><span>✘</span></div>\n  <div class=\"error-message\"><span></span></div>\n</div>"
+      previewTemplate: ""//"<div class=\"preview file-preview\">\n  <div class=\"details\">\n   <div class=\"filename\"><span></span></div>\n  </div>\n  <div class=\"progress\"><span class=\"upload\"></span></div>\n  <div class=\"success-mark\"><span>✔</span></div>\n  <div class=\"error-mark\"><span>✘</span></div>\n  <div class=\"error-message\"><span></span></div>\n</div>"
     };
 
     function Dropzone(element, options) {
