af9b7e180a370c112f104e6476531d51ac9cf7f2
Craig Peden <craig.peden.mail@gmail.com>
2013-04-06 07:08:20 +0100

    Audio and Text fixes

--- application/views/text.php
+++ application/views/text.php
@@ -15,19 +15,7 @@
 	    		<p>Size: <?= $file_size;?> Kb</p>
 	    	</div>
 	    <div class="span9 download-buttons pull-right">
-			<div class="btn-group dropup pull-right download-button">
-			  <button class="btn btn-large btn-info dropdown-toggle" data-toggle="dropdown">
-			    Other <span class="caret"></span>
-			  </button>
-			  <ul class="dropdown-menu">
-			  	<li><a href="#">AVI</a></li>
-			  	<li><a href="#">FLV</a></li>
-			  	<li><a href="#">M4V</a></li>
-			  </ul>
-	    	</div>
-			<a href="#" class="btn btn-large btn-info pull-right download-button"><i class="icon-white icon-download"></i> RTF</a>
-			<a href="#" class="btn btn-large btn-info pull-right download-button"><i class="icon-white icon-download"></i> TXT</a>
-			<a href="<?= $file; ?>" class="btn btn-large btn-info pull-right download-button"><i class="icon-white icon-download"></i> Native</a>
+			<a href="<?= base_url($file_name); ?>" class="btn btn-large btn-info pull-right download-button"><i class="icon-white icon-download"></i> Native</a>
 			<h2 class="pull-right download-label" style="display:block-inline;">Download as:</h2>		
 		</div>
     </div>
